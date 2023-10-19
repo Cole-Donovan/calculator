c53a62a6093ecbd502458f6af1eddf75c89e246e
Cole <coledonovan@gmail.com>
2023-10-19 05:30:51 -0500

    Minor style changes
    Flexbox size adjustments, font and border adjustments

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const buttons = document.getElementsByClassName('button-box');
+
+buttons.addEventListener('mouseover', function (e) {
+    e.target.style.background = '#6643b5';
+});
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
